9fee517fe713db9e3ac5ccc3114d95bbbe3dc759
George Kargiotakis <kargig@void.gr>
2011-10-28 19:55:42 +0300

    o Add some content to index page  o Add url for static content (css)  o Fix some styling

--- urls.py
+++ urls.py
@@ -7,13 +7,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^$', main_page ),
     (r'^grrbl/', include('grrbl.urls')),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':
+        '/home/kargig/grrbl_django/templates/static/'}),
 )
 urlpatterns += patterns('',
     (r'^admin/', include(admin.site.urls))
